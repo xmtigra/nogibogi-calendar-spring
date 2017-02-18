5d9485930e41183f943b75ad7ce21f23467df079
Andriy Pyvovarchuk <andriy.pyvovrchuk@gmail.com>
2017-02-18 15:45:51 +0200

    done footer and header and sponsor sections

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,20 @@
 console.log('\'Allo \'Allo!');
+
+$(window).scroll(function(){
+  var sticky = $('.nb-header'),
+      scroll = $(window).scrollTop();
+
+  if (sticky && sticky.length > 0 && scroll >= 50) {
+    sticky.addClass('nb-header_fixed');
+  } else {
+    sticky.removeClass('nb-header_fixed');
+  }
+});
+
+
+$('body').on('click', '.nb-menu__toggle', function () {
+  $(this).toggleClass('nb-menu__toggle_active');
+  $(this).closest('.nb-header').find('.nb-menu').toggle();
+  $(this).closest('.nb-header').toggleClass('nb-header_opened');
+
+});
